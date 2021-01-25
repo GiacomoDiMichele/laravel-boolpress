eebd95352dbea73f15d9f2d0bd9e6f811dd0d76a
Giacomo <giacomo.1936@gmail.com>
2021-01-25 17:24:17 +0100

    caricati dati tabella tramite Faker

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Post;
+use Faker\Generator as Faker;
 
 class PostsTableSeeder extends Seeder
 {
@@ -9,8 +11,14 @@ class PostsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+         for ($i=0; $i < 10; $i++) {
+           $new_post = new Post();
+           $new_post->title = $faker->word();
+           $new_post->description = $faker->text();
+           $new_post->publication_date = $faker->dateTime();
+           $new_post->save();
+       }
     }
 }
